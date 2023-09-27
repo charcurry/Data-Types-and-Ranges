e13f213eccf0ae6ad2d2eb256a781de869b6ab4e
Charlie <charlieryancurry@gmail.com>
2023-09-27 16:17:07 -0300

    add loop

--- Data Types and Ranges/Program.cs
+++ Data Types and Ranges/Program.cs
@@ -10,6 +10,12 @@ namespace Data_Types_and_Ranges
     {
         static void Main(string[] args)
         {
+            //this for loop will loop from 0 to the max value of int, then overflow to the min value of int, but I set the number higher so this can be seen earlier
+            for (int i = 2147383647; i <= int.MaxValue; i++)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
         }
     }
 }
